c37b0e48000747170ddb2a1aea97191007f3b4ae
kyardley <klynt.yardley@gmail.com>
2016-10-09 16:11:45 -0600

    Initialized Checkpoint1 Project "Blow Out"

---
+++ Blow Out/Blow Out/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Blow_Out.MvcApplication" Language="C#" %>
---
+++ Blow Out/Blow Out/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Blow_Out.Startup))]
+namespace Blow_Out
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

4936e6966382cd99b8c977f927904b8774507585
kyardley <klynt.yardley@gmail.com>
2016-10-17 15:17:47 -0600

    Added Model in class (10/17/16)

---
+++ TestMyKnowledge/TestMyKnowledge/Models/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestMyKnowledge.Models
+{
+    public class Person
+    {
+        public string SSN { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}

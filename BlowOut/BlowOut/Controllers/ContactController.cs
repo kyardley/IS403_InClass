7b20d14b50bd3e95580ac34ae21262a8557218dc
kyardley <klynt.yardley@gmail.com>
2016-10-16 23:49:29 -0600

    Created ContactController

---
+++ BlowOut/BlowOut/Controllers/ContactController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BlowOut.Controllers
+{
+    public class ContactController : Controller
+    {
+        // GET: Contact
+        public String Index()
+        {
+            string contactMessage = "Please call Support at 801-555-1212. Thanks you!";
+            return contactMessage;
+        }
+    }
+}

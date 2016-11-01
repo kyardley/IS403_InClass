f4073702c5002bd65ed796ace7c64d784c9eb85f
kyardley <klynt.yardley@gmail.com>
2016-10-31 18:23:31 -0600

    Testing

---
+++ FirstWebApplication/FirstWebApplication/Controllers/MidtermController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstWebApplication.Controllers
+{
+    public class MidtermController : Controller
+    {
+        // GET: Midterm
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}

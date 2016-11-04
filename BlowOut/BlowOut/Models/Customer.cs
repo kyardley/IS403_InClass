bf08c3ea3d34e24a6c304fef961e9caa778d5e36
kyardley <klynt.yardley@gmail.com>
2016-11-04 14:56:27 -0600

    Created Models

--- BlowOut/BlowOut/Models/Customer.cs
+++ BlowOut/BlowOut/Models/Customer.cs
@@ -11,15 +11,20 @@ namespace BlowOut.Models
     public class Customer
     {
         [Key]
-        public int CustomerID { get; set; }
-        public string Cust_First_Name { get; set; }
-        public string Cust_Last_Name { get; set; }
-        public string Cust_Adress { get; set; }
+        public int Cust_ID { get; set; }
+        [Required(ErrorMessage="Please enter a First Name")]
+        public string Cust_FirstName { get; set; }
+        [Required(ErrorMessage = "Please enter a Last Name")]
+        public string Cust_LastName { get; set; }
+        [Required(ErrorMessage = "Please enter a Street Address")]
+        public string Cust_Address { get; set; }
+        [Required(ErrorMessage = "Please enter a City")]
         public string Cust_City { get; set; }
+        [Required(ErrorMessage = "Please enter a State")]
         public string Cust_State { get; set; }
+        [Required(ErrorMessage = "Please enter a Zip Code")]
         public string Cust_Zip { get; set; }
+        [Required(ErrorMessage = "Please enter a Phone Number")]
         public string Cust_Phone { get; set; }
-
-        
     }
 }
--- BlowOut/BlowOut/Models/Instrument.cs
+++ BlowOut/BlowOut/Models/Instrument.cs
@@ -1,11 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
 namespace BlowOut.Models
 {
+    [Table("Instrument") ]
     public class Instrument
     {
+        [Key]
+        public int Inst_ID { get; set; }
+        [Required(ErrorMessage = "Please enter a Description")]
+        public string Inst_Description { get; set; }
+        [Required(ErrorMessage = "Please enter a Price")]
+        public string Inst_Price { get; set; }
+
+        public string Cust_ID { get; set; }
     }
 }

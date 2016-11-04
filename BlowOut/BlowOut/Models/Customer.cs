9faeaf6f75324081c3a76a4d39d94c29f2c6c4b1
kyardley <klynt.yardley@gmail.com>
2016-11-04 14:41:24 -0600

    Created Connection String

---
+++ BlowOut/BlowOut/Models/Customer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace BlowOut.Models
+{
+    [Table("Customer")]
+    public class Customer
+    {
+        [Key]
+        public int CustomerID { get; set; }
+        public string Cust_First_Name { get; set; }
+        public string Cust_Last_Name { get; set; }
+        public string Cust_Adress { get; set; }
+        public string Cust_City { get; set; }
+        public string Cust_State { get; set; }
+        public string Cust_Zip { get; set; }
+        public string Cust_Phone { get; set; }
+
+        
+    }
+}

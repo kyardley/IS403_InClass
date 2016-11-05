5538b2694956441dcf9a5b4125744353d86dcc17
kyardley <klynt.yardley@gmail.com>
2016-11-05 17:11:26 -0600

    Added Email to Forms

--- BlowOut/BlowOut/Controllers/HomeController.cs
+++ BlowOut/BlowOut/Controllers/HomeController.cs
@@ -41,7 +41,7 @@ namespace BlowOut.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Rental_Form([Bind(Include = "Cust_ID,Cust_FirstName,Cust_LastName,Cust_Address,Cust_City,Cust_State,Cust_Zip,Cust_Phone")] Customer customer, int id)
+        public ActionResult Rental_Form([Bind(Include = "Cust_ID,Cust_FirstName,Cust_LastName,Cust_Address,Cust_City,Cust_State,Cust_Zip,Cust_Email,Cust_Phone")] Customer customer, int id)
         {
             if (ModelState.IsValid)
             {

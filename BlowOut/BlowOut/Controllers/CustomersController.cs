1aff9b78086f228d9d167f00f33800c4df7999e7
kyardley <klynt.yardley@gmail.com>
2016-11-10 22:19:58 -0700

    CheckMate

--- BlowOut/BlowOut/Controllers/CustomersController.cs
+++ BlowOut/BlowOut/Controllers/CustomersController.cs
@@ -47,7 +47,7 @@ namespace BlowOut.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Cust_ID,Cust_FirstName,Cust_LastName,Cust_Address,Cust_City,Cust_State,Cust_Zip,Cust_Email,Cust_Phone")] Customer customer)
+        public ActionResult Create([Bind(Include = "Cust_ID,Cust_FirstName,Cust_LastName,Cust_Address,Cust_City,Cust_State,Cust_Zip,Cust_Phone")] Customer customer)
         {
             if (ModelState.IsValid)
             {
@@ -79,7 +79,7 @@ namespace BlowOut.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Cust_ID,Cust_FirstName,Cust_LastName,Cust_Address,Cust_City,Cust_State,Cust_Zip,Cust_Email,Cust_Phone")] Customer customer)
+        public ActionResult Edit([Bind(Include = "Cust_ID,Cust_FirstName,Cust_LastName,Cust_Address,Cust_City,Cust_State,Cust_Zip,Cust_Phone")] Customer customer)
         {
             if (ModelState.IsValid)
             {
---
+++ SchoolFun/SchoolFun/DAL/HardKnoxContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using SchoolFun.Models;
+
+namespace SchoolFun.DAL
+{
+    public class HardKnoxContext : DbContext
+    {
+        public HardKnoxContext(): base("HardKnoxContext")
+        {
+           
+        }
+
+        public DbSet<Student_Majors> Students_Majors { get; set; }
+        public DbSet<Majors> Major { get; set; }
+
+        public System.Data.Entity.DbSet<SchoolFun.Models.Student> Students { get; set; }
+
+    }
+}
